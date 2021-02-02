7710c1c392659606eb5ca8a994c061b573686eff
DrosoCode <internet-site@outlook.com>
2021-02-02 23:39:19 +0100

    Test auto cache deletion on new update

--- pwa/sw_init.js
+++ pwa/sw_init.js
@@ -1,7 +1,28 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw_content.js', { scope: '/' }).then(function (registration) {
-    console.log('ServiceWorker registration successful with scope:', registration.scope)
-  }).catch(function (error) {
-    console.log('ServiceWorker registration failed:', error)
-  })
+  navigator.serviceWorker.register('/sw_content.js', { scope: '/' }).then(reg => {
+    reg.onupdatefound = () => {
+      const installingWorker = reg.installing
+      installingWorker.onstatechange = () => {
+        if (installingWorker.state === 'installed') {
+          if (navigator.serviceWorker.controller) {
+            // new update available
+            this.$q.notify({
+              message: 'Updating application ...',
+              icon: 'sync',
+              position: 'bottom-left',
+              color: 'light-blue'
+            })
+
+            caches.keys().then(function (keyList) {
+              return Promise.all(keyList.map(function (key) {
+                return caches.delete(key)
+              })).then(function () {
+                window.location.reload(true)
+              })
+            })
+          }
+        }
+      }
+    }
+  }).catch(err => console.error('[SW ERROR]', err))
 }
